c6778bc2e98bfbd868e5c7eff57218c104a570d6
Arthur <arthur4456645@gmail.com>
2024-06-06 23:59:45 +0800

    chore: add ui frameworks & plugins

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import '@/assets/css/tailwind.css'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/reset.css'
 import App from './App.vue'
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(createPinia()).use(Antd)
 
 app.mount('#app')
